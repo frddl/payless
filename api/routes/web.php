77f135eb94f094eda47018f942f66028188f1747
Farid Mammadov <faridmmmdv@gmail.com>
2018-12-06 00:03:36 +0400

    quick update on GET /

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    return 'OK';
 });
 
 $router->post('register', function (Illuminate\Http\Request $request) {
