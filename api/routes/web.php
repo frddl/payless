bc45d90dcf12d67d8bc0079cc3c203e8f0e083c9
Farid Mammadov <faridmmmdv@gmail.com>
2018-12-06 01:05:32 +0400

    added APIs for predefined and user categories

--- api/routes/web.php
+++ api/routes/web.php
@@ -43,6 +43,59 @@ $router->post('login', function (Illuminate\Http\Request $request) {
     return ($result) ? ['response' => '1', 'auth_token' => $token] : ['response' => '0', 'reason' => $reason];
 });
 
+$router->get('/predefinedCategories', function () {
+    return app('db')->table('predefined_categories')->select()->get()->toJson();
+});
+
+$router->get('/userCategories/{token}', function ($token) {
+    if (app('db')->table('users')->where('token', $token)->exists()) {
+        $id = json_decode(app('db')->table('users')->where('token', $token)->get()->toJson(), true)[0]['id'];
+        return app('db')->table('user_categories')->where('user_id', $id)->get()->toJson();
+    } else {
+        return ['response' => '0', 'reason' => 'Not authorized'];
+    }
+});
+
+$router->post('/userCategories/{token}/add', function (Illuminate\Http\Request $request, $token) {
+    if (app('db')->table('users')->where('token', $token)->exists()) {
+        $id = json_decode(app('db')->table('users')->where('token', $token)->get()->toJson(), true)[0]['id'];
+        $cat_name = $request->input('cat_name');
+
+        try {
+            $result = app('db')->table('user_categories')->insert(
+                [   
+                    'user_id' => $id,
+                    'name' => $cat_name,
+                    'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+                    'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+                ]
+            );
+
+            return ['response' => '1'];
+    
+        } catch (\Illuminate\Database\QueryException $e) {
+            $reason = $e->errorInfo[1] . " - " . $e->errorInfo[2];
+        }   
+    } else {
+        return ['response' => '0', 'reason' => 'Not authorized'];
+    }
+});
+
+$router->post('/userCategories/{token}/remove', function (Illuminate\Http\Request $request, $token) {
+    if (app('db')->table('users')->where('token', $token)->exists()) {
+        $id = $request->input('cat_id');
+
+        try {
+            $result = app('db')->table('user_categories')->where('id', $id)->delete();
+            return ['response' => '1'];
+        } catch (\Illuminate\Database\QueryException $e) {
+            $reason = $e->errorInfo[1] . " - " . $e->errorInfo[2];
+        }   
+    } else {
+        return ['response' => '0', 'reason' => 'Not authorized'];
+    }
+});
+
 function generateRandomString($length = 50)
 {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
