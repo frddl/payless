83777d10ef4fa8f82a2b31ca8dbc13a422b19164
Farid Mammadov <faridmmmdv@gmail.com>
2018-12-06 00:02:43 +0400

    added token for users, created APIs for login and register

--- api/database/migrations/2018_12_05_233346_create_users_table.php
+++ api/database/migrations/2018_12_05_233346_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('token');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
         });
--- api/routes/web.php
+++ api/routes/web.php
@@ -14,3 +14,65 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->post('register', function (Illuminate\Http\Request $request) {
+    $email = $request->input('email');
+    $password = $request->input('password');
+
+    $result = 0;
+    $token = generateRandomString();
+
+    try {
+        $result = app('db')->table('users')->insert(
+            [
+                'email' => $email,
+                'password' => md5($password),
+                'token' => $token,
+                'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+                'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+            ]
+        );
+
+    } catch (\Illuminate\Database\QueryException $e) {
+        $reason = $e->errorInfo[1] . " - " . $e->errorInfo[2];
+    }
+
+    return ($result) ? ['response' => '1', 'auth_token' => $token] : ['response' => '0', 'reason' => $reason];
+});
+
+$router->post('login', function (Illuminate\Http\Request $request) {
+    $email = $request->input('email');
+    $password = $request->input('password');
+
+    $result = 0;
+    $token = generateRandomString();
+
+    if (app('db')->table('users')->where('email', $email)->exists()) {
+        try {
+            $result = app('db')->table('email')->where('password', md5($token))->update(
+                [
+                    'token' => $token,
+                    'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+                ]
+            );
+
+        } catch (\Illuminate\Database\QueryException $e) {
+            $reason = $e->errorInfo[1] . " - " . $e->errorInfo[2];
+        }
+    }
+
+    return ($result) ? ['response' => '1', 'auth_token' => $token] : ['response' => '0', 'reason' => $reason];
+});
+
+function generateRandomString($length = 50)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+
+    return $randomString;
+}
