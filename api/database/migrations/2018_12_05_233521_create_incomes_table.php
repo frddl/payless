ac8ffef1f04c0333efd041be938693d29d088c46
Farid Mammadov <faridmmmdv@gmail.com>
2018-12-05 23:41:32 +0400

    created database migrations for users, expenses and incomes

---
+++ api/database/migrations/2018_12_05_233521_create_incomes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIncomesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incomes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->increments('id');
+            $table->integer('category');
+            $table->string('description');
+            $table->float('amount');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incomes');
+    }
+}
